baa078f473e92f61331f7b5becd3db40d205e746
Peacho <emahilmo@>
2023-11-11 18:25:18 -0700

    Add all the steps

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,27 +3,32 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-let fullName = "Emma Hilmo";
-let currentYear = "2023";
-let profilePicture = "images/seniorPhoto.jpg";
-
+let fullName = 'Emma Hilmo';
+let currentYear = 2023;
+let profilePicture = 'images/seniorPhoto.jpg';
 
 /* Step 3 - Element Variables */
-
-
-
-
+const nameElement =  document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year');
+const imageElement = document.getElementById('img');
 
 /* Step 4 - Adding Content */
-
-
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
-
-
+const favoriteFood = ['Pasta ', 'Icecream ', 'Soup ', 'Pizza ', 'Salad'];
+foodElement.innerHTML = favoriteFood;
+const otherFood = 'Ham';
+favoriteFood.push(otherFood);
+foodElement.innerHTML += `<br>${favoriteFood}`;
+favoriteFood.shift();
+foodElement.innerHTML += `<br>${favoriteFood}`;
+favoriteFood.pop();
+foodElement.innerHTML += `<br>${favoriteFood}`;
 
 
 
