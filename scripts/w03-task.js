49da9bdbcb16559d3fea469775594334746ef459
Peacho <emahilmo@>
2023-11-25 22:09:44 -0700

    Finishing off the rest of the code

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -44,17 +44,25 @@ function divideNumbers() {
 document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
 
 /* Decision Structure */
-
+const getTotal = function() {
+    let subtotal = Number(document.querySelector('#subtotal').value);
+    if (document.getElementById("member").checked) {    
+        subtotal *= .80
+    }
+    document.querySelector("#total").textContent = `$ ${subtotal.toFixed(2)}`;
+}
+document.querySelector('#getTotal').addEventListener('click', getTotal);
 
 /* ARRAY METHODS - Functional Programming */
 /* Output Source Array */
-
+let numbersArray = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+document.querySelector('#array').innerHTML = numbersArray;
 /* Output Odds Only Array */
-
+document.querySelector('#odds').innerHTML = numbersArray.filter(number => number % 2 === 1);
 /* Output Evens Only Array */
-
+document.querySelector('#evens').innerHTML = numbersArray.filter(number => number % 2 === 0);
 /* Output Sum of Org. Array */
-
+document.querySelector('#sumOfArray').innerHTML = numbersArray.reduce((sum, number) => sum + number);
 /* Output Multiplied by 2 Array */
-
+document.querySelector('#multiplied').innerHTML = numbersArray.map(number => number * 2);
 /* Output Sum of Multiplied by 2 Array */
