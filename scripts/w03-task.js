2f375291d10fc23a88365e4c95bb678b5c7c262c
Peacho <emahilmo@>
2023-11-25 21:54:17 -0700

    Fixed all sections so far

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -13,27 +13,35 @@ function addNumbers() {
 document.querySelector('#addNumbers').addEventListener('click', addNumbers);
 
 /* Function Expression - Subtract Numbers */
-function Subtract(number1, number2) {
+function subtract(number1, number2) {
     return number1 - number2;
 }
 function subtractNumbers() {
     let subtract1 = Number(document.querySelector('#subtract1').value);
     let subtract2 = Number(document.querySelector('#subtract2').value);
-    document.querySelector('#difference').value = Subtract(subtract1, subtract2);
+    document.querySelector('#difference').value = subtract(subtract1, subtract2);
 }
 document.querySelector('#subtractNumbers').addEventListener('click', subtractNumbers);
 
 /* Arrow Function - Multiply Numbers */
 const multiply = (number1, number2) => number1 * number2;
 const multiplyNumbers = () => {
-    Number(document.querySelector('#factor1').value);
-    Number(document.querySelector('#factor2').value);
+    const factor1 = Number(document.querySelector('#factor1').value);
+    const factor2 = Number(document.querySelector('#factor2').value);
     document.querySelector('#product').value = multiply(factor1, factor2);
 }
 document.querySelector('#multiplyNumbers').addEventListener('click', multiplyNumbers);
 
 /* Open Function Use - Divide Numbers */
-
+function divide(number1, number2) {
+    return number1 / number2;
+}
+function divideNumbers() {
+    let dividend = Number(document.querySelector('#dividend').value);
+    let divisor = Number(document.querySelector('#divisor').value);
+    document.querySelector('#quotient').value = divide(dividend, divisor);
+}
+document.querySelector('#divideNumbers').addEventListener('click', divideNumbers);
 
 /* Decision Structure */
 
