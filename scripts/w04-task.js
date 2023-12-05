29a575fedef13cb4eb767d905c2ebfb7dfb46b0b
Peacho <emahilmo@>
2023-12-04 19:15:21 -0700

    Added profile object and places lived object

---
+++ scripts/w04-task.js
@@ -0,0 +1,44 @@
+/* LESSON 3 - Programming Tasks */
+
+/* Profile Object  */
+let myProfile = {
+    name: "Emma Hilmo",
+    photo: 'images/seniorPhoto.jpg',
+    favoriteFoods: [
+        "Pasta",
+        "Salad",
+        "Sushi",
+        "Hamburger",
+        "Bacon"
+    ],
+    hobbies: [
+        "Dungeons and Dragons",
+        "Minecraft",
+        "Acting",
+        "Hanging out with friends"
+    ],
+    placesLived: [],
+};
+/* Populate Profile Object with placesLive objects */
+myProfile.placesLived.push(
+    {
+        place: "Lehi, UT",
+        length: "21 years",
+    }
+);
+/* DOM Manipulation - Output */
+
+/* Name */
+
+/* Photo with attributes */
+
+
+/* Favorite Foods List*/
+
+
+/* Hobbies List */
+
+
+/* Places Lived DataList */
+
+
